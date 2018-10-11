a47bf0e4a513e359845c716e7433c27c081883fd
byGeek <byyang@mail.dlut.edu.cn>
2018-10-11 15:37:33 +0800

    init

---
+++ src/RadioButtonTest2/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioButtonTest2
+{
+    public enum RW
+    {
+        Read, Write
+    }
+
+    public class TestClass
+    {
+
+        public RW ReadOrWrite { get; set; }
+    }
+}
---
+++ src/RadioButtonTest2/TestViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioButtonTest2
+{
+    public class TestViewModel : ViewModelBase
+    {
+        private readonly TestClass testclass;
+        public TestViewModel(TestClass tc)
+        {
+            testclass = tc;
+        }
+
+        public bool IsRead
+        {
+            get { return testclass.ReadOrWrite == RW.Read; }
+            set
+            {
+                testclass.ReadOrWrite = value ? RW.Read : RW.Write;
+                OnPropertyChanged("IsRead");
+            }
+        }
+
+        public bool IsWrite
+        {
+            get { return testclass.ReadOrWrite == RW.Write; }
+            set
+            {
+                testclass.ReadOrWrite = value ? RW.Write : RW.Read;
+                OnPropertyChanged("IsWrite");
+            }
+        }
+    }
+}
